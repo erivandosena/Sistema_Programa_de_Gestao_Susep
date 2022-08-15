23769007db8c5bfa9ffd2f2330b08c38ebf1bf32
Erivando Sena <erivandosena@gmail.com>
2022-08-15 17:22:47 -0300

    Melhorias ref initialization SqlServer

--- pgd-susep/src/Susep.SISRH.WebApi/Program.cs
+++ pgd-susep/src/Susep.SISRH.WebApi/Program.cs
@@ -1,17 +1,19 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.EventLog;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using System;
+using System.IO;
 
 namespace Susep.SISRH.WebApi
 {
     /// <summary>
-    /// Classe de execucao do programa
+    /// Classe de execução do programa
     /// </summary>
     public class Program
     {
         /// <summary>
-        /// Constroi o programa inicial
+        /// Constrói o programa inicial
         /// </summary>
         /// <param name="args"></param>
         public static void Main(string[] args)
@@ -26,11 +28,14 @@ namespace Susep.SISRH.WebApi
         /// <returns></returns>
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                   .ConfigureLogging(logging =>
-                   {
-                       logging.ClearProviders();
-                       //logging.AddEventLog(new EventLogSettings() { Filter = (source, level) => level == LogLevel.Error });
-                   })
+                   .UseEnvironment(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development")
+                   .ConfigureAppConfiguration((hostingContext, config) =>
+                       config.SetBasePath(Path.Combine(hostingContext.HostingEnvironment.ContentRootPath, "Settings"))
+                             .AddJsonFile($"connectionstrings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                             .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                             .AddJsonFile($"messagebroker.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true)
+                             .AddEnvironmentVariables())
+                   .UseSerilog((context, logger) => logger.ReadFrom.Configuration(context.Configuration))
                    .UseStartup<Startup>();
     }
 }
--- sqlserver/sql-codes/restore-database.sql
+++ sqlserver/sql-codes/restore-database.sql
@@ -1,4 +1,4 @@
-USE [pgd_susep]
+ALTER LOGIN [admin] WITH DEFAULT_DATABASE=[pgd_susep], DEFAULT_LANGUAGE=[Português (Brasil)], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
 GO
 
 -- Cria novo BD staging (testes/suporte)
@@ -7,4 +7,14 @@ GO
 
 -- Cria novo BD treinamento/usuários
 RESTORE DATABASE [pgd_train] FROM  DISK = N'/var/opt/mssql/data/pgd_susep.bak' WITH  FILE = 1,  MOVE N'pgd_susep' TO N'/var/opt/mssql/data/pgd_train.mdf',  MOVE N'pgd_susep_log' TO N'/var/opt/mssql/data/pgd_train_log.ldf',  NOUNLOAD,  REPLACE,  STATS = 5
+GO
+
+USE [pgd_staging]
+GO
+ALTER AUTHORIZATION ON DATABASE::[pgd_staging] TO [admin]
+GO
+
+USE [pgd_train]
+GO
+ALTER AUTHORIZATION ON DATABASE::[pgd_train] TO [admin]
 GO
